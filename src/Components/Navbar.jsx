18389cf64d15aaae6fa0e4e8883ccf2b4b30029b
Divy Jain <divi@Divys-MacBook-Pro.local>
2025-02-14 15:27:11 +0530

    1st update

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Components-CSS/Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <header>
+        <nav>
+            <div className="logo-holder">Divy Kumar Jain</div>
+            <ul className="Nav-UL"> 
+                <li>Home</li>
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Projects</li>
+                <li>Socials</li>
+            </ul>
+        </nav>
+    </header>
+    
+    
+    </>
+  )
+}
+
+export default Navbar
